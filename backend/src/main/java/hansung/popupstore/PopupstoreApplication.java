2a1e59981249e47685128c70dd3486954ab0dc44
sangwoo4 <shangou97@gamil.com>
2024-06-27 18:30:37 +0900

    first commit

---
+++ backend/src/main/java/hansung/popupstore/PopupstoreApplication.java
@@ -0,0 +1,11 @@
+package hansung.popupstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopupstoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PopupstoreApplication.class, args);
+	}
+}
