501ccb1462871db4eb267239915f01ac375a7ecc
Your Name <you@example.com>
2024-08-07 02:27:36 +0900

    로그인 화면 수정

--- frontend/src/pages/Company/Com_Home/Home_Company.js
+++ frontend/src/pages/Company/Com_Home/Home_Company.js
@@ -51,6 +51,8 @@ const Home_Company = () => {
     return <p>Error: {error.message}</p>;
   }
 
+  const baseUrl = 'http://localhost:8080';
+
   return (
     <div>
       <h1>기업 전용 페이지입니다.</h1>
@@ -67,7 +69,7 @@ const Home_Company = () => {
                     location.popupImages.map((image, index) => (
                       <img
                         key={index}
-                        src={image.imageUrl} // 서버에서 제공하는 이미지 URL을 직접 사용
+                        src={`${baseUrl}${image.imageUrl}`} // 서버에서 제공하는 이미지 URL에 도메인을 붙여 사용
                         alt={`Image ${index + 1}`}
                         className="company-popup-image"
                       />
@@ -98,8 +100,12 @@ const Home_Company = () => {
 export default Home_Company;
 
 
+
+
+
+// // 24.08.07 이미지 파일 주소 변환 전
 // import React, { useEffect, useState } from 'react';
-// import { Link } from "react-router-dom";
+// import { Link } from 'react-router-dom';
 // import './Home_Company.css';
 
 // const Home_Company = () => {
@@ -110,13 +116,12 @@ export default Home_Company;
 //   useEffect(() => {
 //     const fetchLocations = async () => {
 //       try {
-//         const token = localStorage.getItem("token");
-
+//         const token = localStorage.getItem('token');
 //         const response = await fetch('http://localhost:8080/company/popuplist', {
-//           method: "GET",
+//           method: 'GET',
 //           headers: {
 //             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`
+//             'Authorization': `Bearer ${token}`,
 //           },
 //         });
 
@@ -126,7 +131,7 @@ export default Home_Company;
 
 //         const data = await response.json();
 //         console.log(data); // 데이터 구조 확인용
-
+        
 //         if (Array.isArray(data.data)) {
 //           setLocations(data.data);
 //         } else {
@@ -163,116 +168,20 @@ export default Home_Company;
 //           locations.map(location => (
 //             <div key={location.id} className='company-popup-item'>
 //               <Link to={`/popup/company/detail/${location.id}`} className='company-popup-link'>
-              
 //                 <div className="company-category-box">
-//                   {location && location.popupImages && location.popupImages.length > 0 ? (
-//                     location.popupImages.map((imageUrl, index) => (
-//                       <img key={index} src={imageUrl} alt={`Popup ${index + 1}`} className="company-popup-image" />
+//                   {location.popupImages && location.popupImages.length > 0 ? (
+//                     location.popupImages.map((image, index) => (
+//                       <img
+//                         key={index}
+//                         src={image.imageUrl} // 서버에서 제공하는 이미지 URL을 직접 사용
+//                         alt={`Image ${index + 1}`}
+//                         className="company-popup-image"
+//                       />
 //                     ))
 //                   ) : (
 //                     <img src="/images/image1.png" alt="Default Image" className="company-popup-image" />
 //                   )}
 //                 </div>
-
-//                 <div className='company-popup-details'>
-//                   <h3>{location.title}</h3>
-//                   <p>{location.description}</p>
-//                   <div className="company-category-box">
-//                     {location.categories && location.categories.map((category, index) => (
-//                       <div key={index} className="company-category-item">{category.category}</div>
-//                     ))}
-//                   </div>
-//                 </div>
-//               </Link>
-//               <Link to={`/popup/company/update/${location.id}`} className='company-popup-edit-link'>수정하기</Link>
-//             </div>
-//           ))
-//         )}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Home_Company;
-
-
-
-
-
-
-
-
-// // 24.08.07 이미지 수정 전 코드
-// import React, { useEffect, useState } from 'react';
-// import { Link } from "react-router-dom";
-// import './Home_Company.css';
-
-// const Home_Company = () => {
-//   const [locations, setLocations] = useState([]);
-//   const [loading, setLoading] = useState(true);
-//   const [error, setError] = useState(null);
-
-//   useEffect(() => {
-//     const fetchLocations = async () => {
-//       try {
-//         const token = localStorage.getItem("token");
-
-//         const response = await fetch('http://localhost:8080/company/popuplist', {
-//           method: "GET",
-//           headers: {
-//             'Content-Type': 'application/json',
-//             'Authorization': `Bearer ${token}`
-//           },
-//         });
-
-//         if (!response.ok) {
-//           throw new Error(`Network response was not ok: ${response.statusText}`);
-//         }
-
-//         const data = await response.json();
-//         console.log(data); // 데이터 구조 확인용
-
-//         if (Array.isArray(data.data)) {
-//           setLocations(data.data);
-//         } else {
-//           console.error("Received data does not contain an array of locations:", data);
-//           setLocations([]);
-//         }
-//         setLoading(false);
-//       } catch (error) {
-//         console.error('Error fetching data:', error);
-//         setError(error);
-//         setLoading(false);
-//       }
-//     };
-
-//     fetchLocations();
-//   }, []);
-
-//   if (loading) {
-//     return <p>Loading...</p>;
-//   }
-
-//   if (error) {
-//     return <p>Error: {error.message}</p>;
-//   }
-
-//   return (
-//     <div>
-//       <h1>기업 전용 페이지입니다.</h1>
-//       <div className='company-popup-list-wrapper'>
-//         <h2>현재 등록한 팝업</h2>
-//         {locations.length === 0 ? (
-//           <p>등록된 팝업스토어가 없습니다.</p>
-//         ) : (
-//           locations.map(location => (
-//             <div key={location.id} className='company-popup-item'>
-//               <Link to={`/popup/company/detail/${location.id}`} className='company-popup-link'>
-//                 <img 
-//                   src={location.image ? location.image : "/images/image1.png"} 
-//                   alt={location.title} 
-//                   className='company-popup-image'
-//                 />
 //                 <div className='company-popup-details'>
 //                   <h3>{location.title}</h3>
 //                   <p>{location.description}</p>
--- frontend/src/pages/Login/LoginPage.js
+++ frontend/src/pages/Login/LoginPage.js
@@ -5,22 +5,64 @@ import { Link } from 'react-router-dom';
 export default function LoginPage() {
   return (
     <div className="selection-page">
-      <div className="title">환영합니다!</div>
-      <div className="subtitle">원하는 옵션을 선택하세요</div>
-      <div className="button-group">
-        <Link to="/auth/company/login">
-          <button className="option-button">기업 로그인</button>
-        </Link>
-        <Link to="/auth/company/signup">
-          <button className="option-button">기업 회원가입</button>
-        </Link>
-        <Link to="/auth/user/login">
-          <button className="option-button">고객 로그인</button>
-        </Link>
-        <Link to="/auth/user/signup">
-          <button className="option-button">고객 회원가입</button>
-        </Link>
+      <div className="section left">
+        <div className="title">기업</div>
+        <div className="subtitle">로그인 또는 회원가입</div>
+        <div className="button-group">
+          <Link to="/auth/company/login">
+            <button className="option-button">기업 로그인</button>
+          </Link>
+          <Link to="/auth/company/signup">
+            <button className="option-button">기업 회원가입</button>
+          </Link>
+        </div>
+      </div>
+      <div className="section right">
+        <div className="title">고객</div>
+        <div className="subtitle">로그인 또는 회원가입</div>
+        <div className="button-group">
+          <Link to="/auth/user/login">
+            <button className="option-button">고객 로그인</button>
+          </Link>
+          <Link to="/auth/user/signup">
+            <button className="option-button">고객 회원가입</button>
+          </Link>
+        </div>
       </div>
     </div>
   );
 }
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import './LoginPage.css';
+// import { Link } from 'react-router-dom';
+
+// export default function LoginPage() {
+//   return (
+//     <div className="selection-page">
+//       <div className="title">환영합니다!</div>
+//       <div className="subtitle">원하는 옵션을 선택하세요</div>
+//       <div className="button-group">
+//         <Link to="/auth/company/login">
+//           <button className="option-button">기업 로그인</button>
+//         </Link>
+//         <Link to="/auth/company/signup">
+//           <button className="option-button">기업 회원가입</button>
+//         </Link>
+//         <Link to="/auth/user/login">
+//           <button className="option-button">고객 로그인</button>
+//         </Link>
+//         <Link to="/auth/user/signup">
+//           <button className="option-button">고객 회원가입</button>
+//         </Link>
+//       </div>
+//     </div>
+//   );
+// }
