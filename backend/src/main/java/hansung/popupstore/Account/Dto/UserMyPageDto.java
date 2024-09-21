7af850a4c256404a0422511fea6f29358ca10f75
Doyun Jang <ehdbsdl0304@gmail.com>
2024-09-22 01:24:19 +0900

    최종 마이페이지 수정 업로드

--- backend/src/main/java/hansung/popupstore/Account/Dto/UserMyPageDto.java
+++ backend/src/main/java/hansung/popupstore/Account/Dto/UserMyPageDto.java
@@ -17,7 +17,7 @@ public class UserMyPageDto {
     private String email;
     private List<Map<String, Object>> reservations;
     private List<HeartDto> hearts;
-    private List<UserMyPageReviewDto> reviews;
+    private List<UserMyPageReviewDto> reviews;  // 09/22 수정한 라인
     private List<String> categories;
     private int allHearts;
     private int allReviews;
